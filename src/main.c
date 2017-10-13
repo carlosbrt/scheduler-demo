d75fe6f65035f4f563f37d8e508699678dbab40c
Carlos Bortoloto <carlosbortoloto@gmail.com>
2017-10-13 19:04:06 -0300

    Passa até o test04

--- src/main.c
+++ src/main.c
@@ -6,8 +6,22 @@ int main() {
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
+  
+
   for (int i=0; i<n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
+
+    for (int j=0; j<5; j++) {
+
+      if (((i+1)%a[j])==0){
+
+        printf ("%d", j);
+
+      }
+
+    }
+    printf("\n");
+
   }
 
   return 0;
